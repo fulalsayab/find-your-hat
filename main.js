0ffddc98ecccbfaaaddfa3270eabde897cd3bc2f
falsaya <falsaya@ncsu.edu>
2021-05-31 03:16:41 -0400

    added print method and field class

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require("prompt-sync")({ sigint: true });
 
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
+
+class Field {
+  constructor(field) {
+    this.field = field;
+  }
+
+  print() {
+    this.field.forEach((element) => {
+      console.log(element.join(""));
+    });
+  }
+}
+
+const myField = new Field([
+  ["*", "░", "O"],
+  ["░", "O", "░"],
+  ["░", "^", "░"],
+]);
+
+console.log(myField.print());
